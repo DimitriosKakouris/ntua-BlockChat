4fcea60670c3ab2df78e4c0f0907cc3dcfe085c1
olgacode <olgabarlou@gmail.com>
2024-01-28 16:19:38 +0200

    Create Node.go

---
+++ src/Node.go
@@ -0,0 +1,24 @@
+package main
+
+
+type Node struct {
+	address 			string
+	wallet  			Wallet
+	ip 					string
+	port 				int
+	id 					int
+	blockchain 			Blockchain
+	pendingTransactions []Transaction
+	// ... other fields
+}
+
+
+func create_new_block(transactions *Transaction, bc *Blockchain) *Block {
+	// Create a new block
+	block := NewBlock(bc.length, transactions, bc.last_block.current_hash)
+	// Add the block to the blockchain
+	bc.add_block(block)
+	// Reset the pending transactions
+	bc.pendingTransactions = []Transaction{}
+	return block
+}
